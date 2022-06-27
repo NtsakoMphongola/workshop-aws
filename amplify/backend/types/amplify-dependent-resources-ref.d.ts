7163e2f639c2bf76b8d2ed1540046a6b8da88341
NtsakoMphongola <105928245+NtsakoMphongola@users.noreply.github.com>
2022-06-27 10:32:09 +0200

    initial

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsworkshop0c978f61": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
